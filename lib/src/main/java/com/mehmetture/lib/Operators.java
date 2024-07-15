4b56543f8c03ee6bd6bc728ab1ad137df0e4beed
turememo43 <ture.memo.7@gmail.com>
2024-07-15 11:13:50 +0300

    operators

---
+++ lib/src/main/java/com/mehmetture/lib/Operators.java
@@ -0,0 +1,35 @@
+package com.mehmetture.lib;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int[] myNumbers = {12,15,18,21,24};
+
+        for (int i = 0; i < myNumbers.length; i++){
+
+            int x = myNumbers[i] / 3 * 5;
+          //  System.out.println(x);
+        }
+
+        for (int number : myNumbers){
+            System.out.println(number / 3 * 5);
+        }
+
+
+        // WHILE
+
+        int j = 0;
+
+        while ( j< 10 ) {
+
+            int m = j * 20;
+            System.out.println(m);
+            j++;
+
+        }
+
+
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Project9_Operators"
+include(":app")
+include(":lib")
